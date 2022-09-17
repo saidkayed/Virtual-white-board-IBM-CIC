c323ce583f1a14a270ae837efdb0ac56306a3a9c
Your Name <you@example.com>
2022-09-17 17:23:26 +0200

    add comment to post fixed and working

--- Whiteboard-API/Whiteboard-API/Migrations/20220917151804_updatedModel.Designer.cs
+++ Whiteboard-API/Whiteboard-API/Migrations/20220917151804_updatedModel.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Whiteboard_API.Data;
 
@@ -11,9 +12,10 @@ using Whiteboard_API.Data;
 namespace Whiteboard_API.Migrations
 {
     [DbContext(typeof(Whiteboard_APIContext))]
-    partial class Whiteboard_APIContextModelSnapshot : ModelSnapshot
+    [Migration("20220917151804_updatedModel")]
+    partial class updatedModel
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
